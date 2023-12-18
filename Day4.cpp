6da296f7dbdee3f43ba4cbd693e85f49f3699bde
Ayush Kumar <ayushsuperstar48@gmail.com>
2023-12-18 18:40:26 +0530

    Update Day4.cpp

--- Day4.cpp
+++ Day4.cpp
@@ -4,47 +4,46 @@
 #include <array>
 using namespace std;
 
-int getBit(int n, int pos){
-        return ((n&(1<<pos))!=0);
-    }
+int getBit(int n, int pos)
+{
+    return ((n & (1 << pos)) != 0);
+}
 int main()
 {
-    string str="dsdjsbdjcjnsd";
+    string str = "dsdjsbdjcjnsd";
     for (int i = 0; i < str.size(); i++)
     {
-        if (str[i]>='a' && str[i]<='z')
+        if (str[i] >= 'a' && str[i] <= 'z')
         {
-            str[i]-=32;
+            str[i] -= 32;
         }
-        
     }
-    
-    cout<<str<<endl;
-    
-    for (int i = 0; i < str.size(); i++)
-    {
-        if (str[i]>='A' && str[i]<='Z')
-        {
-            str[i]+=32;
-        }
-        
-    }
-    
-    cout<<str<<endl;
-    transform(str.begin(),str.end(),str.begin(),::toupper);
-    cout<<str<<endl;
-    string str="125643";
-    sort(str.begin(),str.end(),greater<int>());
-    cout<<str;
 
-    string str="sapejjednoqfkjjjwlklj";
+    cout << str << endl;
+
+    for (int i = 0; i < str.size(); i++)
+    {
+        if (str[i] >= 'A' && str[i] <= 'Z')
+        {
+            str[i] += 32;
+        }
+    }
+
+    cout << str << endl;
+    transform(str.begin(), str.end(), str.begin(), ::toupper);
+    cout << str << endl;
+    string str = "125643";
+    sort(str.begin(), str.end(), greater<int>());
+    cout << str;
+
+    string str = "sapejjednoqfkjjjwlklj";
     int freq[26];
-    
+
     for (int i = 0; i < 26; i++)
     {
         /* code */
     }
-    
-    cout<<getBit(5,2)<<endl;
+
+    cout << getBit(5, 2) << endl;
     return 0;
 }
