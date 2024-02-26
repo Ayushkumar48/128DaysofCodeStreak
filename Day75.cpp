cf81585ab6c2aa89174ef61c2cdef03ae2d19e07
Ayush Kumar <ayushsuperstar48@gmail.com>
2024-02-26 23:11:42 +0530

    Create Day75.cpp

---
+++ Day75.cpp
@@ -0,0 +1,47 @@
+//LeetCode
+
+// 905. Sort Array By Parity
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int i=0;
+        int j=nums.size()-1;
+        while(i<=j){
+            if(nums[i]%2!=0){
+                swap(nums[i], nums[j]);
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        return nums;
+    }
+};
+
+// 304. Find N Unique Integers Sum up to Zero
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        if (n % 2 == 0) {
+            vector<int> ans(n);
+            for (int i = 0; i < n / 2; i++) {
+                ans[i] = i + 1;
+            }
+            for (int i = n / 2; i < n; i++) {
+                ans[i] = -1 * ans[i - (n / 2)];
+            }
+            return ans;
+        }
+        n--;
+        vector<int> ans(n);
+        for (int i = 0; i < n / 2; i++) {
+            ans[i] = i + 1;
+        }
+        for (int i = n / 2; i < n; i++) {
+            ans[i] = -1 * ans[i - (n / 2)];
+        }
+        ans.push_back(0);
+        return ans;
+    }
+};
